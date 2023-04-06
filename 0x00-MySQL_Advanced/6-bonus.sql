436ae6b5f26d0ffdfaf83c4519f042522bac2fae
mathjken <ogujohnkennedy@gmail.com>
2023-04-06 22:42:38 +0000

    task 6 and 8

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,18 @@
--- this SQL script creates a stored procedure AddBonus
+--this SQL script that creates a stored procedure AddBonus
 -- that adds a new correction for a student.
 --@author Ogu Johnkennedy
-DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER |
 CREATE PROCEDURE AddBonus (
     IN user_id int,
     IN project_name varchar(255),
-    IN score int
+    IN score float
 )
 BEGIN
     INSERT INTO projects (name)
     SELECT project_name FROM DUAL
 
     WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
-    	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END;
 |
