039c3fe3847caedd3ac11d5d8c057e04bdc10a23
mathjken <ogujohnkennedy@gmail.com>
2023-04-12 23:32:43 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,3 +21,23 @@ class Cache:
         key: uuid.UUID = uuid.uuid1()
         self._redis.set(str(key), data)
         return str(key)
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """Retrieves a value from a Redis data storage.
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """Retrieves a string value from a Redis data storage.
+        """
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Retrieves an integer value from a Redis data storage.
+        """
+        return self.get(key, lambda x: int(x))
